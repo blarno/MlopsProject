501ab85fe46c8dc395b1af44968032633d733178
blarno <bigsquare@mail.ru>
2023-12-24 17:11:50 +0300

    Initial commit

---
+++ source/train.py
@@ -0,0 +1,86 @@
+from subprocess import check_output
+
+from dvc.api import DVCFileSystem
+from hydra import main
+from joblib import dump
+from mlflow import log_metric, log_param, set_tracking_uri, start_run
+from numpy import mean
+from omegaconf import DictConfig
+from pandas import read_csv
+from sklearn.metrics import mean_absolute_percentage_error
+from toml import load
+from torch import from_numpy, nn, optim, utils
+from torcheval.metrics import R2Score
+
+
+class SetUpData(utils.data.Dataset):
+    def __init__(self, path_file):
+        data_matrix = self.df = read_csv(path_file, index_col=0)
+        data_matrix = from_numpy(data_matrix)
+        self.X = data_matrix[:, 1:13]
+        self.X = self.data.float()
+        self.y = data_matrix[:, 0]
+
+        self.n_samples = self.data.shape[0]
+
+    def __len__(self):  # Length of the dataset.
+        return self.X.size()[0]
+
+    def __getitem__(self, index):
+        return self.data[index], self.target[index]
+
+
+@main(version_base=None, config_path="../conf", config_name="config")
+def train_model(cfg: DictConfig):
+    config_path = "mlflow_config.toml"
+    config = load(config_path)
+    set_tracking_uri(f"http://{config['server']['host']}:{config['server']['port']}")
+    git_commit_id = (
+        check_output(["git", "rev-parse", "--short", "HEAD"]).strip().decode("utf-8")
+    )
+    with start_run():
+        log_param("git_commit_id", git_commit_id)
+        log_param("batch_size", cfg["params"].batch_size)
+        log_param("learning_rate", cfg["params"].learning_rate)
+        log_param("epochs", cfg["params"].epochs)
+        fs = DVCFileSystem("https://github.com/blarno/mlops_project1/")
+        with fs.open("data/titanic.csv") as f:
+            data = SetUpData(f)
+        train_size = int(0.8 * len(data))
+        test_size = len(data) - train_size
+
+        train, test = utils.data.random_split(data, [train_size, test_size])
+        data_train = utils.data.DataLoader(train, batch_size=cfg["params"].batch_size)
+        dump(data.X[test.indices], "data/X_test.h5")
+
+        model = nn.Sequential(
+            nn.Linear(12, 6),
+            nn.ReLU(),
+            nn.Linear(6, 1),
+            nn.Sigmoid(),
+        )
+
+        loss_fn = nn.MSELoss()
+        optimizer = optim.Adam(model.parameters(), lr=cfg["params"].learning_rate)
+        for i in range(cfg["params"].epochs):
+            losses = []
+            mape = []
+            r2score = []
+            for X, y in data_train:
+                pred = model(X).view(-1)
+                loss = loss_fn(pred, y)
+
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+                losses.append(loss.detach().numpy())
+                mape.append(mean_absolute_percentage_error(pred.detach().numpy(), y))
+                r2score.append(R2Score().update(pred, y).compute())
+            log_metric("MAPE_train", mean(mape), step=i)
+            log_metric("R2Score_train", mean(r2score), step=i)
+            log_metric("MSELoss_train", mean(losses), step=i)
+        dump(model, "models/model.h5")
+
+
+if __name__ == "__main__":
+    train_model()
